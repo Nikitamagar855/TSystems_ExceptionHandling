9cda5cde9ee72cef6ae8396b385c4c2318b8c469
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 21:38:08 +0530

    Add project files.

---
+++ TSystems_ExceptionHandling/Exception1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_ExceptionHandling
+{
+     public class Exception1:Exception
+    {
+
+        public Exception1(String msg) : base(msg)
+        {
+
+        }
+    }
+
+    public class user
+    {
+        public int AcceptAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new Exception1("this user is not valid for vote");
+            }
+
+            return age;
+        }
+    }
+}
---
+++ TSystems_ExceptionHandling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_ExceptionHandling
+{
+   internal class Program
+  public   class Program
+    {
+       
+        static void Main(string[] args)
+        {
+             @@ -39,7  +39,17 @@ 
+        Console.WriteLine("---------------custom Exception-----------------");
+            user u = new user();
+            try
+            {
+                int age = u.AcceptAge(13);
+                Console.WriteLine("your  age is "+age);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+    }
+
+    }
+}
